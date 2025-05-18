b3fb06e9152d5dfaa707a8a0e6f0fbc61f817721
Dana Barhoom <127206851+Danab19@users.noreply.github.com>
2025-05-18 16:45:51 +0100

    Add files via upload

---
+++ lab12/lab12App.swift
@@ -0,0 +1,17 @@
+//
+//  lab12App.swift
+//  lab12
+//
+//  Created by Fadi Barhoom on 12/29/24.
+//
+
+import SwiftUI
+
+@main
+struct lab12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
